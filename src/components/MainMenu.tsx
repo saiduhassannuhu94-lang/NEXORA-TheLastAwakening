c3916bf43aa25144b2f0865807dd6ce5e7f11b4b
saiduhassannuhu94-lang <saiduhassannuhu94@gmail.com>
2026-08-05 01:09:38 +0100

    Add MainMenu component with game options

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,1 +1,49 @@
+type Props = {
+  onNewGame: () => void;
+};
 
+export default function MainMenu({ onNewGame }: Props) {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#050816",
+        color: "white",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "70px",
+          color: "#00e5ff",
+          marginBottom: "10px",
+        }}
+      >
+        NEXORA
+      </h1>
+
+      <h2 style={{ marginBottom: "40px" }}>
+        The Last Awakening
+      </h2>
+
+      <button onClick={onNewGame}>
+        ▶ New Game
+      </button>
+
+      <button style={{ marginTop: 15 }}>
+        Continue
+      </button>
+
+      <button style={{ marginTop: 15 }}>
+        Settings
+      </button>
+
+      <button style={{ marginTop: 15 }}>
+        Exit
+      </button>
+    </div>
+  );
+            }
